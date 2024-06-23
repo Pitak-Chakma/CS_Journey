1adfcb7c3022a0059952e87c7bbe9a775a623a6b
Pitak-Chakma <131612585+Pitak-Chakma@users.noreply.github.com>
2024-06-23 20:55:02 +0600

    chapter 3 starts

---
+++ CSC-305/3.UserInput + TypeConversion/UserInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your Name: ");
+        String name = input.nextLine();
+        System.out.println("Good Morning " + name);
+        System.out.print("Tell me your age: ");
+        int age = input.nextInt();
+        System.out.println("Your age is " +age);
+    }
+}
--- CSC-305/README.md
+++ CSC-305/README.md
@@ -1,1 +1,5 @@
 ## CSC-305 + Lab (Object Oriented Programming)
+
+Files starting with "Ex" are basically Practice Programs
+Others contains info/theory/examples related to Java
+
