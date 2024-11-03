2e14c742067ef993bf26084d6fa850576a3b39b6
Pitak W <pitakchakmacse@gmail.com>
2024-11-03 22:04:23 +0600

    multi arguments + concatination

---
+++ CSC-101+Lab/Week_0_Function_Variables/variable2.py
@@ -0,0 +1,7 @@
+# Ask user for thier name
+name = input("What's Your Name? ")
+
+# Say Hello to User - using single arguments (Concatination)
+print("Hello, " + Name)
+
+
---
+++ CSC-101+Lab/Week_0_Function_Variables/variable3_multiArgumets.py
@@ -0,0 +1,5 @@
+# Ask user for thier name
+name = input("What's Your Name? ")
+
+# Say Hello to User  - using multi argument
+print("Hello, ", name) 
