22bf9d3c6f96e96fdbb42c252ade08b3b56c84b7
Pitak W <pitakchakmacse@gmail.com>
2024-11-03 22:07:48 +0600

    typo fix

--- CSC-101+Lab/Week_0_Function_Variables/variable2_Concatenation.py
+++ CSC-101+Lab/Week_0_Function_Variables/variable2_Concatenation.py
@@ -1,7 +1,7 @@
 # Ask user for thier name
 name = input("What's Your Name? ")
 
-# Say Hello to User - using single arguments (Concatination)
+# Say Hello to User - using single arguments (Concatenation)
 print("Hello, " + Name)
 
 
