0dfbcd91a06ccda40977c088b614a35cb7fda188
Pitak W <pitakchakmacse@gmail.com>
2024-11-04 17:55:33 +0600

    overriding variations

---
+++ CSC-101+Lab/Week_0_Function_Variables/variable4_Overriding_end.py
@@ -0,0 +1,8 @@
+name = input("What's Your Name? ")
+
+# Say Hello to User  - using multi argument
+print("Hello, ")
+print(name) 
+
+print("Hello,", end=" ")
+print(name) 
---
+++ CSC-101+Lab/Week_0_Function_Variables/variable5_Overriding_sep.py
@@ -0,0 +1,3 @@
+name = input("What's Your Name? ")
+
+print("Hello,",name,sep="")
