ad18312f95cc7d65c0cd51d4cf1710a8d3a32884
Pitak <131612585+Pitak-Chakma@users.noreply.github.com>
2024-12-03 13:37:45 +0600

    variable start

---
+++ CSC-101+Lab/S3_Variable_Strings/variables_Can_Be_.py
@@ -0,0 +1,13 @@
+# Assigned to other variables 
+first_variable = 10
+another_variable = first_variable + 20
+
+print(another_variable)
+
+# reassigned 
+first_variable = 20
+print(first_variable)
+
+# assigned at the same time as other variables 
+all,at,once = 10,20,30
+print(all,at,once)
