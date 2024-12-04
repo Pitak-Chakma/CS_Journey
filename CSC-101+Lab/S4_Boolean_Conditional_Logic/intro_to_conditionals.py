b26ca75b552f16fcd619ce115963fc678372950a
Pitak <131612585+Pitak-Chakma@users.noreply.github.com>
2024-12-04 23:37:30 +0600

    if else elif

---
+++ CSC-101+Lab/S4_Boolean_Conditional_Logic/intro_to_conditionals.py
@@ -0,0 +1,8 @@
+name = input("what is your name?: ")
+
+if name == "Arya Stark":
+    print("Valar Morghulis")
+elif name == "Jon Snow":
+    print("You know nothing")
+else:
+    print("Carry On")
---
+++ CSC-101+Lab/S4_Boolean_Conditional_Logic/multiple_elif.py
@@ -0,0 +1,10 @@
+colour = input("What is your fav colour?: ")
+
+if colour == "red":
+    print("Great")
+elif colour == "Black":
+    print("Classy")
+elif colour == "White":
+    print("Cool")
+else:
+    print("Awesome")
