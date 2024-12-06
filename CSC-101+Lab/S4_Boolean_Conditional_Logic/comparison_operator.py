ce1d6c3ce1d5c5334dcdb727db12dba2ae9aeca7
Pitak M <131612585+Pitak-Chakma@users.noreply.github.com>
2024-12-06 14:21:59 +0600

    operators

---
+++ CSC-101+Lab/S4_Boolean_Conditional_Logic/comparison_operator.py
@@ -0,0 +1,16 @@
+'''
+Comparison Operators - returns Booelean
+== 
+!= 
+> 
+< 
+>= 
+<=
+'''
+
+age = 17
+
+if age >= 18:
+    print("You can vote")
+else:
+    print("You cannot vote")
---
+++ CSC-101+Lab/S4_Boolean_Conditional_Logic/logical_operators.py
@@ -0,0 +1,8 @@
+'''
+logical operators 
+and = if both true then true 
+or = if either true then true
+not = reverse the true and false
+'''
+
+# not only works with one bolean value
