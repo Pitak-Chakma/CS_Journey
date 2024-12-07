aaac2ae4d6a5b2b06f4340faa149ec47d8892cbf
Pitak M <131612585+Pitak-Chakma@users.noreply.github.com>
2024-12-07 19:11:15 +0600

    nested_loop + smily Pyramid

--- CSC-101+Lab/S5_Loops/S5_Programs/smily_pyramid.py
+++ CSC-101+Lab/S5_Loops/S5_Programs/smily_pyramid.py
@@ -1,4 +1,18 @@
+print("---- For Loop ----")
 
 for i in range(1,10):
     print("\U0001f600" * i)
 
+print("---- While Loop ----")
+num = 1
+while num<10:
+    print("\U0001f600" * num)
+    num += 1
+
+print("---- Both Loop ----")
+
+num = 1
+for i in range(1,10): 
+    while num<i+1:
+        print("\U0001f600" * num)
+        num = num + 1
---
+++ CSC-101+Lab/S5_Loops/nested_loops.py
@@ -0,0 +1,4 @@
+for times in range(2):
+    for letter in "Pitak":
+        print(letter*3)
+    print(f"{times+1} completed!!")
